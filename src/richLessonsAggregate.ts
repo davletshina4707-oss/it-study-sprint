ed8c0b3f49648af56d62573d264087a9fcd1ff43
davletshina4707-oss <davletshina47.07@icloud.com>
2026-09-02 20:43:54 +0300

    Add later-semester textbook modules to lesson resolver

--- src/richLessonsAggregate.ts
+++ src/richLessonsAggregate.ts
@@ -1,9 +1,11 @@
 import { getRichLesson as getCoreRichLesson } from './richLessons'
 import { getDayOneRichLesson } from './dayOneLessons'
 import { getAllSubjectRichLesson } from './allSubjectRichLessons'
+import { getFutureRichLesson } from './futureRichLessons'
 
 export function getRichLesson(subjectId:string,lessonId:string){
   return getCoreRichLesson(subjectId,lessonId)
     || getDayOneRichLesson(subjectId,lessonId)
     || getAllSubjectRichLesson(subjectId,lessonId)
+    || getFutureRichLesson(subjectId,lessonId)
 }

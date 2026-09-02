eede461db5d7dc8bbab2f5c19d32d19cf50d06d6
davletshina4707-oss <davletshina47.07@icloud.com>
2026-09-02 19:41:23 +0300

    Wire first-day textbook modules into app build

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'rich-lessons-aggregate',
+      transform(code, id) {
+        if (id.endsWith('/src/main.tsx')) {
+          return code.replace("from './richLessons'", "from './richLessonsAggregate'")
+        }
+        return null
+      },
+    },
+  ],
 })

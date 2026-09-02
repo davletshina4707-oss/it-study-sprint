c4aaf00a8c133aa61f627cde97a24aec4c86ea40
davletshina4707-oss <davletshina47.07@icloud.com>
2026-09-02 20:18:46 +0300

    Use textbook modules across all study subjects

--- src/richLessonsAggregate.ts
+++ src/richLessonsAggregate.ts
@@ -1,6 +1,9 @@
 import { getRichLesson as getCoreRichLesson } from './richLessons'
 import { getDayOneRichLesson } from './dayOneLessons'
+import { getAllSubjectRichLesson } from './allSubjectRichLessons'
 
 export function getRichLesson(subjectId:string,lessonId:string){
-  return getCoreRichLesson(subjectId,lessonId) || getDayOneRichLesson(subjectId,lessonId)
+  return getCoreRichLesson(subjectId,lessonId)
+    || getDayOneRichLesson(subjectId,lessonId)
+    || getAllSubjectRichLesson(subjectId,lessonId)
 }
